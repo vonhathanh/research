e7d5bbe8298b310ccf700d2fa42d743d585de2e3
hanhvn <vonhathanh@gmail.com>
2024-09-12 09:59:25 +0700

    codes and notes for chapter 14

--- zk/chapter_14.py
+++ zk/chapter_14.py
@@ -1,17 +1,51 @@
-from py_ecc.bn128 import G1, multiply, add, neg, eq
+import unittest
+from functools import reduce
 
-# Prover
-x = 5
+import galois
+from py_ecc.bn128 import G1, multiply, add, neg, eq, curve_order, Z1
 
-X3 = multiply(G1, 5**3)
-X2 = multiply(G1, 5**2)
-X = multiply(G1, 5)
 
-# Verifier
-left_hand_side = multiply(G1, 39)
-right_hand_side = add(add(add(multiply(X3, 1),
-                              multiply(neg(X2), 4)),
-                              multiply(X, 3)),
-                              multiply(neg(G1), 1))
+class TestChapter14(unittest.TestCase):
+    def test_exponent_encryption(self):
+        # polynomial: 39 = x^3 - 4x^2 + 3x - 1
+        # Prover
+        x = 5
 
-assert eq(left_hand_side, right_hand_side), "lhs ≠ rhs"
+        X3 = multiply(G1, 5**3)
+        X2 = multiply(G1, 5**2)
+        X = multiply(G1, 5)
+
+        # Verifier
+        left_hand_side = multiply(G1, 39)
+        right_hand_side = add(add(add(multiply(X3, 1),
+                                      multiply(neg(X2), 4)),
+                                      multiply(X, 3)),
+                                      multiply(neg(G1), 1))
+
+        assert eq(left_hand_side, right_hand_side), "lhs ≠ rhs"
+
+    def test_polynomial_over_ff(self):
+        print("initializing a large field, this may take a while...")
+        GF = galois.GF(curve_order)
+
+        def inner_product(ec_points, coeffs):
+            return reduce(add, (multiply(point, int(coeff)) for point, coeff in zip(ec_points, coeffs)), Z1)
+
+        def generate_powers_of_tau(tau, degree):
+            return [multiply(G1, int(tau ** i)) for i in range(degree + 1)]
+
+        # p = (x - 4) * (x + 2)
+        p = galois.Poly([1, -4], field=GF) * galois.Poly([1, 2], field=GF)
+
+        # evaluate at 8
+        tau = GF(8)
+
+        # evaluate then convert
+        powers_of_tau = generate_powers_of_tau(tau, p.degree)
+        evaluate_then_convert_to_ec = multiply(G1, int(p(tau)))
+
+        # evaluate via encrypted evaluation# coefficients need to be reversed to match the powers
+        evaluate_on_ec = inner_product(powers_of_tau, p.coeffs[::-1])
+
+        if eq(evaluate_then_convert_to_ec, evaluate_on_ec):
+            print("elliptic curve points are equal")
--- zk/chapter_14_encrypted_polynomial_evaluation.md
+++ zk/chapter_14_encrypted_polynomial_evaluation.md
@@ -3,4 +3,6 @@
 - Let's say we want to prove we know x such that: x^3 - 4x^2 + 3x - 1 = 0
 - We can't give x to the verifier, so we encrypt it with G1, x' = xG1. The problem is the verifier can't compute
 x^3 due to pairing can't be used twice, so our solution is to calculate all encrypted x, x^2, x^3 and give it to the verifier
+- Trusted setup: typically, we use the above construction in reverse. Mainly because this algorithm requires the verifier
+to do work linearly proportional to the size of the polynomial. We want succinct proof not a linear one
 - 
