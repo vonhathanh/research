6ad8ab5501aa32ee4df14c780adb7b88cf5ddd68
hanhvn <vonhathanh@gmail.com>
2024-09-14 14:05:51 +0700

    code for chapter 12

--- zk/chapter_12.py
+++ zk/chapter_12.py
@@ -1,41 +1,78 @@
 import unittest
 
 import numpy as np
+from py_ecc.bn128 import multiply, G1
 from scipy.interpolate import lagrange
 
+# prover: I know the result of this polynomial: out = (x_1)^2 + 4(x_2)^2(x_1) - 2
+# break the above equation to quadratic constraints:
+# x_3 = x_1*x_1
+# x_4 = x_2*x_2
+# out - x_3 +2 = 4x_4*x_1
+# witness vectors s: [1, out, x_1, x_2, x_3, x_4]
+# Our L, R, O matrices must have 3 rows and 6 columns
+# Apply the left-right hand rule we learned in chapter 10, we got
+# L = [[0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 4]]
+# R = [[0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0], [0, 0, 1, 0, 0, 0]]
+# O = [[0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 1], [2, 1, 0, 0, -1, 0]]
+# Next, we transform the columns from vectors to polynomial
+# Start with L, zero vectors turn to zero polynomials, we'll skip doing that here
+# With vector [1, 0, 0], we need a polynomial that travel through (1, 1), (2, 0), (3, 0)
+xs = [1, 2, 3]
+ys = [[1, 0, 0], [0, 1, 0], [0, 0, 4]]
+for y in ys:
+    p = lagrange(xs, y)
+    print(p.c)
 
-class TestQAP(unittest.TestCase):
+ys = [[1, 0, 1], [0, 1, 0]]
+for y in ys:
+    p = lagrange(xs, y)
+    print(p.c)
 
-    def test_lagrange_interpolation(self):
-        x = np.array([1,2,3])
-        y = np.array([4,12,6])
+ys = [[0, 0, 2], [0, 0, 1], [1, 0, -1], [0, 1, 0]]
+for y in ys:
+    p = lagrange(xs, y)
+    print(p.c)
 
-        polyC = lagrange(x, y)
+# everything we've done so far is public, what remain hidden is the witness vector, let's pick x_1 = 3, x_2 = 4
+# so our witness vector is [1, 199, 3, 4, 9, 16]
+Ua = np.array([[0, 0, 0.5, -1, 0, 2],
+                [0, 0, -2.5, 4, 0, -6],
+                [0, 0, 3, -3, 0, 4]])
 
-        print(polyC)
-        # -7x^2 + 29x - 18# check that it passes through x = {1,2,3} as expected
-        print(polyC(1))
-        # 4.0
-        print(polyC(2))
-        # 12.0
-        print(polyC(3))
-        # 6.0
+witness = [1, 199, 3, 4, 9, 16]
 
-    def test_homomorphism(self):
-        x = np.array([1, 2, 3])
-        y_v1 = np.array([1, 0, 1])
-        y_v2 = np.array([-1, 5, 3])
-        poly_v1 = lagrange(x, y_v1)
-        poly_v2 = lagrange(x, y_v2)
+U = (np.matmul(Ua, witness))
 
-        print(poly_v1)
-        # 1 x^2 - 4 x + 4
-        print(poly_v2)
-        # -4 x^2 + 18 x - 15
+Va = np.array([[0, 0, 1, -1, 0, 0],
+               [0, 0, -4, 4, 0, 0],
+               [0, 0, 4, -3, 0, 0]])
 
-        poly_v3 = poly_v1 + poly_v2
+V = (np.matmul(Va, witness))
+# array([-1,  4,  0])
 
-        print(poly_v3)
-        # -3 x^2 + 14 x - 11
-        print([poly_v3(1), poly_v3(2), poly_v3(3)])
-        # [0.0, 5.0, 4.0]
+Wa = np.array([[1, 0.5, 0, 0, 0, -1],
+               [-3, -1.5, 0, 0, -1, 4],
+               [2, 1, 0, 0, 2, -3]])
+
+W = (np.matmul(Wa, witness))
+# array([  84.5, -246.5,  171. ])
+
+u = np.poly1d(U)
+v = np.poly1d(V)
+w = np.poly1d(W)
+
+print(u, v, w)
+
+t = np.poly1d([1, -1]) * np.poly1d([1, -2]) * np.poly1d([1, -3])
+print(t)
+
+h = (u*v - w) / t
+h = h[0]
+
+# last step, pick a random t and perform encrypted polynomial evaluation
+# let's pick t = 5, we got:
+X = multiply(G1, 5)
+X2 = multiply(G1, 5 ** 2)
+X3 = multiply(G1, 5 ** 3)
+X4 = multiply(G1, 5 ** 4)
