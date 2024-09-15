f0f62f6a7a8f262a959904570073c427a617350b
hanhvn <vonhathanh@gmail.com>
2024-09-15 09:34:33 +0700

    code for chapter 13

---
+++ zk/chapter_13.py
@@ -0,0 +1,108 @@
+from functools import reduce
+
+import numpy as np
+import galois
+
+# 1, out, x, y, v1, v2, v3
+L = np.array([
+    [0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, -5, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1],
+])
+
+R = np.array([
+    [0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0],
+])
+
+O = np.array([
+    [0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 1],
+    [0, 1, 0, 0, 0, -1, 0],
+])
+
+x = 4
+y = -2
+v1 = x * x
+v2 = v1 * v1         # x^4
+v3 = -5*y * y
+out = v3*v1 + v2    # -5y^2 * x^2
+
+witness = np.array([1, out, x, y, v1, v2, v3])
+
+assert all(np.equal(np.matmul(L, witness) * np.matmul(R, witness), np.matmul(O, witness))), "not equal"
+
+L = np.array([
+    [0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 74, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1],
+])
+
+R = np.array([
+    [0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0],
+])
+
+O = np.array([
+    [0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 1],
+    [0, 1, 0, 0, 0, 78, 0],
+])
+
+
+GF = galois.GF(79)
+
+L_galois = GF(L)
+R_galois = GF(R)
+O_galois = GF(O)
+
+x = GF(4)
+y = GF(79-2)
+
+v1 = x * x
+v2 = v1 * v1         # x^4
+v3 = GF(79-5)*y * y
+out = v3*v1 + v2    # -5y^2 * x^2
+
+witness = GF(np.array([1, out, x, y, v1, v2, v3]))
+
+assert all(np.equal(np.matmul(L_galois, witness) * np.matmul(R_galois, witness), np.matmul(O_galois, witness))), "not equal"
+
+
+def interpolate_column(col):
+    xs = GF(np.array([1,2,3,4]))
+    return galois.lagrange_poly(xs, col)
+
+
+# axis 0 is the columns. apply_along_axis is the same as doing a for loop over the columns and collecting the results
+# in an array
+U_polys = np.apply_along_axis(interpolate_column, 0, L_galois)
+V_polys = np.apply_along_axis(interpolate_column, 0, R_galois)
+W_polys = np.apply_along_axis(interpolate_column, 0, O_galois)
+
+
+def inner_product_polynomials_with_witness(polys, witness):
+    mul_ = lambda x, y: x * y
+    sum_ = lambda x, y: x + y
+    return reduce(sum_, map(mul_, polys, witness))
+
+
+term_1 = inner_product_polynomials_with_witness(U_polys, witness)
+term_2 = inner_product_polynomials_with_witness(V_polys, witness)
+term_3 = inner_product_polynomials_with_witness(W_polys, witness)
+
+t = galois.Poly([1, 78], field = GF) * galois.Poly([1, 77], field = GF) * galois.Poly([1, 76], field = GF) * galois.Poly([1, 75], field = GF)
+
+h = (term_1 * term_2 - term_3) // t
+
+print(h)
+
+assert term_1 * term_2 == term_3 + h * t, "division has a remainder"
